e3386c01ce085298ef0949dc023548fc9a5a74cb
RISHAV KISHORE <rishavkishore732@gmail.com>
2024-06-13 22:32:07 +0530

    read data from database in compiler

---
+++ MongoDB/read_dta_from_mongo.js
@@ -0,0 +1,12 @@
+const dbConnect=require('./data_database');
+
+const main=async ()=>{
+    let data=await dbConnect();
+    console.log('db connected')
+    data=await data.find({"name":"Rishav"}).toArray();
+    console.log (data);
+}
+main();
+
+
+// why we create another file because this is the right way to read or get the data from the databases
